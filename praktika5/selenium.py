8773f5c31de948ca68229e511ae13273c464908e
milenamilosa <milenamilosh@inbox.lv>
2023-11-26 14:35:40 +0200

    26.11.23

--- praktika5/selenium.py
+++ praktika5/selenium.py
@@ -2,6 +2,7 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service #sakam datoram ka mes stradasim ar hromu
 from selenium.webdriver.common.by import By
+import time
 
 service = Service()
 option = webdriver.ChromeOptions()
@@ -9,6 +10,15 @@ driver = webdriver.Chrome(service=service, options=option) # tieši hroms
 # šeit beidzas iepriekš definētas rindas. jebkuram pārlūkam būs tas pats kods
 url = "https://www.ebay.com"
 driver.get(url) # izsaukt pārlūks
-input() # lai programma neaizvērtu uzreiz tīmekļa vietni kamēr nav uzspiest enter
+time.sleep(2)
+find = driver.find_element(By.ID, "dgpr-banner-accept")
+find.click()
 
+find = driver.find_element(By.LINK_TEXT, "Electronics")
+find.click()
+
+
+
+input() # lai programma neaizvērtu uzreiz tīmekļa vietni kamēr nav uzspiest enter
+# parametri: id / class / link text (<a> hipersaite tīmekļa vietnē)
 
