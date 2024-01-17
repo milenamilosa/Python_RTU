80ac436c0b47a1d715a1e7ce7cfee16945a48d3e
milenamilosa <milenamilosh@inbox.lv>
2024-01-17 14:30:34 +0200

    automatizācijas projekta izstrāde

--- project/231RDB016_1alv_project.py
+++ project/231RDB016_1alv_project.py
@@ -8,23 +8,21 @@ from selenium.webdriver.common.keys import Keys
 import time
 from tabulate import tabulate
 
+item = input("Ievadiet meklējamo preci: ")                      # meklējamās preces ievade no klaviatūras (rekomendējamā ievade: lego)
+# item = "lego"                                                 # atkomentēt, ja negrib vadīt preci no klaviatūras
 
 service = Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
 
-# item = input("Enter item to search:")
-item = "lego"
-
 url = "https://www.1a.lv"
 driver.get(url)
-driver.maximize_window()    # atver mājaslapu "fullscreen" ērtam webskrepingam
+driver.maximize_window()                                        # atver mājaslapu "fullscreen" režīmā
 
-driver.find_element(By.CLASS_NAME,"c-button-inverse").click()   # pieņem cookie failus
-# time.sleep(1)
+driver.find_element(By.CLASS_NAME,"c-button-inverse").click()   # akceptē cookie failus
 
-find=driver.find_element(By.ID, "q")    # atrod meklēšanas logu
-find.send_keys(item)                    # ieraksta preces nosaukumu logā
+find=driver.find_element(By.ID, "q")                            # atrod meklēšanas logu
+find.send_keys(item)                                            # ieraksta preces nosaukumu logā
 time.sleep(1)
 
 driver.find_element(By.CLASS_NAME,"main-search-submit").click() # spiež uz meklēšanas pogu
@@ -39,51 +37,25 @@ time.sleep(1)
 
 price.send_keys(200)                                            # ievada lietotāja budžetu (cenas augstāko robežu)
 price.send_keys(Keys.ENTER)
-time.sleep(5)
+time.sleep(2)
 
-# saraksti ar mājaslapas precēm
-nosaukumi = []
-cenas = []
+nosaukumi = []                  # saraksts ar mājaslapas preču nosaukumiem
+cenas = []                      # saraksts ar mājaslapas preču cenām
 
-# prece = driver.find_element(By.CLASS_NAME, "catalog-taxons-product__name")                # savāc visus cenas elementus pēc klases
-# print(prece.get_attribute('outerHTML'))
-
-preces_nosaukumi = driver.find_elements(By.CLASS_NAME, "catalog-taxons-product__name")      # savāc visus elementu nosaukumus pēc klases
+preces_nosaukumi = driver.find_elements(By.CLASS_NAME, "catalog-taxons-product__name")              # meklē visus elementu nosaukumus pēc klases un veido sarakstu no tiem
 for preces_nosakums in preces_nosaukumi:
     nosaukumi.append((preces_nosakums.get_attribute('innerHTML')).strip())
-    # print((preces_nosakums.get_attribute('innerHTML')))                                     # izvada preces nosaukumu
-# print(nosaukumi)                                                                          # preču nosaukumu saraksts
 
-preces_cenas = driver.find_elements(By.CLASS_NAME, "catalog-taxons-product-price__item-price")      # savāc visus elementu nosaukumus pēc klases
+preces_cenas = driver.find_elements(By.CLASS_NAME, "catalog-taxons-product-price__item-price")      # meklē visus elementu cenas pēc klases un veido sarakstu no tiem
 
-for preces_cena in driver.find_elements(By.XPATH, '//span[@class = "catalog-taxons-product-price__item-price"]'):   # savāc tekstu no 'span'a konkrētās klases elementiem
-    # print (preces_cena.text)
+for preces_cena in driver.find_elements(By.XPATH, '//span[@class = "catalog-taxons-product-price__item-price"]'):   # savāc tekstu no 'span'-a cenas klases elementiem
     cena = preces_cena.text.replace(" ", "")
     cena = cena.replace("€/gab.", "")
     cena = cena.replace(",", ".")
     cena = float(cena)
     cena = float(("{:.2f}".format(cena)))
-    # cena = (format(cena, '.2f'))
-    cenas.append(cena)                                                                # pievieno cenu sarakstam
+    cenas.append(cena)          # pievieno cenu sarakstam
 
-# for preces_cena in preces_cenas:
-#     cenas.append((preces_cena.get_attribute('innerHTML')).strip())
-    # print((preces_cena.get_attribute('innerHTML')))                                     # izvada preces nosaukumu
-# print(cenas)                                                                      # preču nosaukumu saraksts
-
-count = 0
-for i in nosaukumi:
-    count+=1
-# print(count)
-
-count = 0
-for i in cenas:
-    count+=1
-# print(count)
-# for i in preces:
-#     prece = driver.find_element(By.CLASS_NAME, "catalog-taxons-product__name")
-#     prece.get_attribute('innerHTML')
-#     print(prece)
 
 table = zip(cenas, nosaukumi)
-print(tabulate(sorted(table), headers=["Preces nosaukums", "Preces cena"], tablefmt="github"))
+print(tabulate(sorted(table), headers=["Preces nosaukums", "Preces cena"], tablefmt="github"))      # izvada pirmo 48 populārāko preču sarakstu filtrējot no zemākās cenas līdz augstākai
